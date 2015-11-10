5a55a43f35cab96f2613a3802d1f9e8b4712a619
thantalas <th.thantalas@gmail.com>
2015-11-10 08:39:28 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,30 @@
 <?php
-use \Nne;
-//require 'Nne/config.php';
+/**
+ * 
+ * 
+ *
+ * Nne  : Ninety Nine Enemies Project (http://thnet.komunikando.org)
+ * 
+ * Copyright (c) Ninety Nine Enemies Project, (http://thnet.komunikando.org)
+ * Licensed under The MIT License
+ * For license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ * Based on:
+ * SlimStarter, https://github.com/xsanisty/SlimStarter
+ * slim-facades, https://github.com/itsgoingd/slim-facades
+ * 
+ * @copyright	Copyright (c) Ninety Nine Enemies, (http://thnet.komunikando.org)
+ * @link		http://thnet.komunikando.org Ninety Nine Enemies Project
+ * @package		Nne\Controllers
+ * @since		Nne (tm) v 1
+ * @license		http://www.opensource.org/licenses/mit-license.php MIT License
+ * @project		Ninety Nine Enemies Project 
+ * @encoding	utf-8
+ * @author		Giorgio Tonelli <th.thantalas@gmail.com>, <http://thnet.komunikando.org>
+ * @creation	08/nov/2015
+ */
+require 'Nne/config.php';
 require CORE_LIBS_PATH.'commonlib.php';
-$app = require 'app/bootstrap/start.php';
+$app = require APP_PATH.'bootstrap/start.php';
 $app->run();
